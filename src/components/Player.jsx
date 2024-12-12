5a2d56ea699e4e677c735c9e67745b406023335d
Matthew Aitken <matt@itken.net>
2024-12-12 14:37:15 +0000

    Add player names

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+
+    const [ playerName, setPlayerName ] = useState(initialName);
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value)
+    }
+
+    let playerInfo = <span className="player-name">{playerName}</span>
+
+    if (isEditing) {
+        playerInfo = <input type="text" required Value={playerName} onChange={handleChange} />
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {playerInfo}
+                <span className="player_symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
